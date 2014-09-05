76bd8820ba3f478141bd1d057f41c2595797c0ee
Denys Butenko <ua13dark@gmail.com>
2014-09-06 02:23:46 +0300

    Add project/apps to TEMPLATE_DIRS.

--- project/project/settings/base.py
+++ project/project/settings/base.py
@@ -151,6 +151,7 @@ TEMPLATE_LOADERS = (
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
 TEMPLATE_DIRS = (
     normpath(join(SITE_ROOT, 'templates')),
+    normpath(join(SITE_ROOT, 'project', 'apps', 'templates')),
 )
 # END TEMPLATE CONFIGURATION
 
